3ca124b48d5faac412244d10a9b2c9b8054c2704
かみざとよしと <kamizatoyoshito@kamizatoyoshitonoMacBook-Air.local>
2020-08-23 13:58:12 +0900

    最低限完成

--- main.rb
+++ main.rb
@@ -1,15 +1,51 @@
-puts "今日はどの項目を何分勉強しましたか？？"
+require "thor"
+require "pry"
 
-print "学習項目を入力 ＞"
+learning_content_list = []
 
-learning_content = gets.chomp
+loop do
+  puts "今日はどの項目を何分勉強しましたか？？"
 
-print "学習時間を入力 ＞"
+  print "学習項目を入力 ＞"
 
-learning_time = gets.to_i
+  name = gets.chomp
 
-square_num = learning_time / 15
+  print "学習時間を入力 ＞"
 
-puts "学習記録"
+  time = gets.to_i
 
-puts "#{learning_content}　#{"■ " * square_num}"
+
+  learning_content = {name: name, time: time}
+
+  learning_content_name_list = []
+
+  learning_content_list.each do |learning_content|
+    learning_content_name_list << learning_content[:name]
+  end
+
+  if learning_content_name_list.include?(name)
+    learning_content_list.map! do |learning_content|
+      if name == learning_content[:name]
+        {name: name, time: learning_content[:time] + time}
+      else
+        learning_content
+      end
+    end
+  else
+    learning_content_list << {name: name, time: time}
+  end
+
+  puts "学習記録"
+
+  learning_content_list.each do |learning_content|
+    square_num = learning_content[:time] / 15
+    puts "#{learning_content[:name]}　#{"■ " * square_num}"
+  end
+
+  puts "引き続き入力しますか？"
+  puts "YES -> y"
+  puts "No -> n"
+
+  answer = gets.chomp
+  break if answer == "n"
+end
